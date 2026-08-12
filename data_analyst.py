a6ed5dfc9357e9fdfa8373199edba11123025cbb
pawunder1992 <pavelkryvutsa@gmail.com>
2026-08-12 21:22:58 +0300

    Add data visualization for top skills in analysis

--- data_analyst.py
+++ data_analyst.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 data = pd.read_csv("csv/2026-08-12_20-31-54-statistic.csv")
 
 # 1. Збираємо унікальні скіли (ваш код)
@@ -20,6 +20,13 @@ for skill in all_skills:
 skills_df = pd.DataFrame(skills_dict)
 
 data = pd.concat([data, skills_df], axis=1)
-data.to_csv("test.csv", index=False)
 
-print(data["Python"].sum())
+
+ax = data[all_skills].sum().sort_values(ascending=False).head(20).plot.bar(rot=90)
+plt.title("Top 15")
+plt.xlabel("Technology")
+plt.ylabel("Amount")
+plt.tight_layout()
+
+plt.savefig("plots/top_skills_chart.png", dpi=300, bbox_inches='tight')
+
